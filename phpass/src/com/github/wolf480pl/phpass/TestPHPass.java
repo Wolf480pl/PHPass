0b59696482e84056599706d91aa1e016ce28e789
Wolf480pl <wolf480@interia.pl>
2013-01-23 19:12:37 +0100

    Cleaned up the unit test case, added test methods for national characters.

--- phpass/src/com/github/wolf480pl/phpass/TestPHPass.java
+++ phpass/src/com/github/wolf480pl/phpass/TestPHPass.java
@@ -1,40 +1,58 @@
 package com.github.wolf480pl.phpass;
 
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 public class TestPHPass extends TestCase {
-    static String correct = "test12345";
-    static String wrong = "test12346";
-    static String hash = "$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0";
-    static PHPass phpass = new PHPass(8);
+    static final String correct = "test12345";
+    static final String wrong = "test12346";
+    static final String hash = "$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0";
+    static final String correctpl = "ąćęłóńśźż";
+    static final String wrongpl = "acelonszz";
+    static final String hashpl = "$P$ByXWbzDvVAJ0jxvNp5sv4xYPIRXJJ1.";
+    static final PHPass phpass = new PHPass(8);
     String hashed = phpass.HashPassword(correct);
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         junit.textui.TestRunner.run(TestPHPass.class);
     }
+
     @Test
     public final void testCheckPassword_correct() {
         boolean check = phpass.CheckPassword(correct, this.hashed);
         Assert.assertTrue(check);
     }
+
     @Test
     public final void testCheckPassword_wrong() {
         boolean check = phpass.CheckPassword(wrong, this.hashed);
         Assert.assertFalse(check);
     }
+
     @Test
     public final void testCheckPassword_givenhash_correct() {
         boolean check = phpass.CheckPassword(correct, hash);
         Assert.assertTrue(check);
     }
+
     @Test
     public final void testCheckPassword_givenhash_wrong() {
         boolean check = phpass.CheckPassword(wrong, hash);
         Assert.assertFalse(check);
+    }
+
+    @Test
+    public final void testCheckPassword_nationalCharacters_correct() {
+        boolean check = phpass.CheckPassword(correctpl, hashpl);
+        assertTrue(check);
+    }
+
+    @Test
+    public final void testCheckPassword_nationalCharacters_wrong() {
+        boolean check = phpass.CheckPassword(wrongpl, hashpl);
+        assertFalse(check);
     }
 
 }
